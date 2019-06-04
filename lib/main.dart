fad0efb3bbe18988e1ce57de4432c2a450ad0a4c
thelovesmith <averydante9@gmail.com>
2019-06-03 20:39:47 -0400

    started video widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+    
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primaryTextTheme: TextTheme(title: TextStyle(fontSize: 20,
+        color: Colors.black)),
+       
+        primaryColor: Colors.redAccent,
+        highlightColor: Colors.indigo,
+        splashColor: Colors.orange,
+        secondaryHeaderColor: Colors.indigo,
+        
+        colorScheme: ColorScheme.dark(
+          primary: const Color.fromRGBO(150, 22, 122, .5),
+        ),
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -18,9 +31,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        // primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Avery Video Player '),
     );
   }
 }
@@ -54,6 +67,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      print('counter');
+      print(_counter );
     });
   }
 
@@ -70,35 +85,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.amber,
+        
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Text('Video Widget', 
+              style: TextStyle(fontSize: 20),),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
